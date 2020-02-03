2e04b1c3164e2330ef3766b7cdb95b9a2fee8941
claratepa <60242649+claratepa@users.noreply.github.com>
2020-02-03 00:12:54 -0600

    Add files via upload
    Práctica1: Movimiento Browniano

---
+++ Practica1/pr1sim.R
@@ -0,0 +1,42 @@
+registro = data.frame()
+dimension = 8
+potencia = 10
+for (pot in 5:potencia){
+  duracion <- 2^potencia
+  for (dim in 1:dimension) {
+    
+    datos = numeric()
+    repeticiones = 50
+    for (replica in 1:repeticiones) {
+      resultado = FALSE
+      pos <- rep(0, dim)
+      for (t in 1:duracion) {
+        modificar = sample(1:dim, 1)
+        if (runif(1) < 0.5) {
+          pos[modificar] =  pos[modificar] + 1
+        } else {
+          pos[modificar] =  pos[modificar] - 1
+        }
+        if (all(pos == 0)) {
+          resultado = TRUE
+          break
+        }
+      }
+      datos = c(datos, resultado)
+}
+  porc = 100 * sum(datos) / repeticiones
+  registro = rbind(registro, c(pot, porc, dim))
+  }
+}
+names(registro) = c("pot", "porc", "dim")
+png("pr1si.png")
+boxplot(porc ~ dim, 
+data =  registro,
+main="Probabilidad de regreso al punto de origen",
+xlab="Dimensi\u{F3}n",
+ylab="Porcentaje de regreso al punto de origen",
+col=rainbow(8, alpha=0.2),
+border = rainbow(8, v=0.6)
+)
+dev.off()
+
