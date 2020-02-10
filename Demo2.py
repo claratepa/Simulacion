d4862dc942e8bcca4da0bcd2b7d6d521b9db1bbe
claratepa <60242649+claratepa@users.noreply.github.com>
2020-02-10 14:40:13 -0600

    Add files via upload

---
+++ Demo2.py
@@ -0,0 +1,29 @@
+import numpy as np
+from random import random
+import matplotlib.cm as cm
+import matplotlib.pyplot as plt
+
+maximo = 50
+dim = 30
+num = dim**2
+for prob in range(11):
+    prob /= 10
+    for replicas in range(10):
+        valores = [random() < prob for i in range(num)]
+        iteraciones = 0
+        while sum(valores) > 0:
+            actual = np.reshape(valores, (dim, dim))
+            nuevos = []
+            for pos in range(num):
+                fila = (pos - 1) // dim
+                columna = (pos - 1) % dim
+                vecindad = actual[(fila - 1):(fila + 2), (columna - 1):(columna + 2)]
+                nuevos.append(1 * (np.sum(vecindad) - actual[fila, columna] == 3))
+            valores = nuevos
+            iteraciones += 1
+            if iteraciones == maximo:
+                print("matanza obligatoria")
+                break
+        print(prob, replicas, iteraciones)
+
+
