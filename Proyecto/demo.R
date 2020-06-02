9e7ce4c575e2e4c25e9102497c16d5030a507cbe
Elisa Schaeffer <elisa.schaeffer@gmail.com>
2020-06-01 19:54:41 -0500

    Demo aumentado

--- Proyecto/demo.R
+++ Proyecto/demo.R
@@ -5,8 +5,18 @@ for (age in 0:9) {
     y = c(y, runif(1))
     tiempo = paste("Age", age)
     png(paste("demo_", age, ".png", sep=""))
-    plot(x, y, xlim = c(0, 1))
+    plot(x, y, xlim = c(-0.1, 1.1), ylim = c(-0.1, 1.1))
     lines(x, y)
     graphics.off()
 }
-
+d = read.csv('demo.txt', sep=' ', header=FALSE)
+names(d) = c('x', 'y')
+for (age in 1:dim(d)[1]) {
+    x = d$x[1:age]
+    y = d$y[1:age]
+    tiempo = paste("(from file) Age", age)
+    png(paste("fdemo_", age, ".png", sep=""))
+    plot(x, y, xlim = c(-0.1, 1.1), ylim = c(-0.1, 1.1))
+    lines(x, y)
+    graphics.off()
+}
