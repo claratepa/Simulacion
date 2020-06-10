32d00bb7ee574c22ba285e002ad411e69bfa9665
Elisa Schaeffer <elisa.schaeffer@gmail.com>
2020-06-10 09:41:54 -0500

    Efecto artificial

--- Proyecto/histodemo.R
+++ Proyecto/histodemo.R
@@ -1,18 +1,18 @@
 filas = 5
-columnas = 2
+columnas = 4
 cuantos = 1000
 png('histodemo.png', width = 600, height = 800)
 par(mfrow=c(filas, columnas))
 for (f in 1:filas) {
     media = 2 * f
     for (c in 1:columnas) {
-        desv = c / 2
+        desv = sqrt(c)
         titulo = sprintf('Ejemplo %d %d', f, c)
         hist(rnorm(cuantos, media, desv),
              main = titulo,
              col = 'green',
-             xlim = c(-5, 15),
-             ylim = c(0, 400))
+             xlim = c(-5, 20),
+             ylim = c(0, 300))
     }
 }
 graphics.off()
